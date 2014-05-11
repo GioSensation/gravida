b65e6b219e3fd3b75f7b0e1d7c9b9401c7be2906
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2014-05-11 22:39:46 +0200

    Cleanup
    Clean up a few small things, from small css fixes and reorganizations to javascript JSHint compliance and stuff like that.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- scripts.js
+++ scripts.js
@@ -4,18 +4,18 @@ window.addEventListener("load", function () {
 	FastClick.attach(document.body);
 	
 	// Add a polyfill to fix the vh issue in Mobile Safari
-	viewportUnitsBuggyfill.init()
+	viewportUnitsBuggyfill.init();
 	
 	var menuHelper = document.getElementById('menu-helper');
 	
-	menuHelper.addEventListener('click', function(event) {
+	menuHelper.addEventListener('click', function() {
 		this.classList.toggle('menu-helper--open');
 		this.nextElementSibling.classList.toggle('nav--open');
-	})
+	});
 	
 	document.getElementById('nav').addEventListener('click', function() {
 		menuHelper.classList.remove('menu-helper--open');
 		this.classList.remove('nav--open');
-	})
+	});
 	
 })
